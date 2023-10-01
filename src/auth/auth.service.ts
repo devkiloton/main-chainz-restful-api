b42c4725ddccb16d4f1a5c7cdae9303be6c3423e
devkiloton <dev.kiloton@gmail.com>
2023-09-30 21:57:41 -0300

    Creating the auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { UserEntity } from './entities/user.entity';
+import { pickBy } from 'lodash';
+
+@Injectable()
+export class AuthService {
+  private _users: Array<UserEntity> = [];
+
+  public async createOne(user: UserEntity): Promise<void> {
+    this._users.push(user);
+    this._users.find(user => user.id === user.id);
+  }
+
+  public async getOne(id: string): Promise<UserEntity | null> {
+    const user = this._users.find(user => user.id === id);
+    return user ?? null;
+  }
+
+  public async getOneByEmail(email: string): Promise<UserEntity | null> {
+    const user = this._users.find(user => user.email === email);
+    return user ?? null;
+  }
+
+  public async updateOne(data: { id: string; user: Partial<UserEntity> }): Promise<UserEntity | null> {
+    const userObj = await this.getOne(data.id);
+    const userIndex = this._users.findIndex(user => user.id === data.id);
+    if (userObj) {
+      const user = pickBy(data.user, field => field !== undefined);
+      this._users[userIndex] = { ...userObj, ...user };
+    }
+    return this._users[userIndex] ?? null;
+  }
+
+  public async deleteOne(id: string): Promise<UserEntity | null> {
+    const user = await this.getOne(id);
+    this._users = this._users.filter(user => user.id !== id);
+    return user ?? null;
+  }
+}
