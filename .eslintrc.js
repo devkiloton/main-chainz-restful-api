7cd48a23aa26afb8ce582ef8a72f12735a399175
devkiloton <dev.kiloton@gmail.com>
2023-09-30 21:54:22 -0300

    Making the eslint less rigorous

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
