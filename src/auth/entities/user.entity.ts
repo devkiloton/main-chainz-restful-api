9b63c03ee9e0ae650e73615c2e3b955745c2d121
devkiloton <dev.kiloton@gmail.com>
2023-10-01 12:48:26 -0300

    Typeorm in the use entity

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,15 +1,37 @@
-export class UserEntity {
-  public readonly id!: string;
-  public readonly name!: string;
-  public readonly email!: string;
-  public readonly password!: string;
-  public readonly createdAt!: Date;
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-  constructor(name: string, email: string, password: string, createdAt: Date, id: string) {
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid', { name: 'id' })
+  public readonly id!: string;
+  @Column({ name: 'name', nullable: false, length: 100 })
+  public readonly name!: string;
+  @Column({ name: 'email', nullable: false, length: 100 })
+  public readonly email!: string;
+  @Column({ name: 'password', nullable: false, length: 255 })
+  public readonly password!: string;
+  @CreateDateColumn({ name: 'created_at' })
+  public readonly createdAt!: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  public readonly updatedAt!: Date;
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  public readonly deletedAt!: Date | null;
+
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt: Date | null = null,
+  ) {
+    this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
     this.createdAt = createdAt;
-    this.id = id;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
   }
 }
