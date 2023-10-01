136cec1afea4154b5a34b3c3b893bfde35246dba
devkiloton <dev.kiloton@gmail.com>
2023-09-30 22:39:59 -0300

    Setting up configs ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
+import { PostgresService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresService,
+      inject: [PostgresService],
+    }),
+  ],
 })
 export class AppModule {}
