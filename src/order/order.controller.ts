702bb95da2f72283cac1267e3b13777b734ece59
devkiloton <dev.kiloton@gmail.com>
2023-10-01 21:05:03 -0300

    Improving returned values

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -4,6 +4,7 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrderEntity } from './entities/order.entity';
 import { UuidService } from 'src/shared/services/uuid.service';
+import { Response } from 'src/types/response';
 
 @Controller('order')
 export class OrderController {
@@ -13,7 +14,7 @@ export class OrderController {
   ) {}
 
   @Post()
-  async create(@Body() createOrderDto: CreateOrderDto): Promise<void> {
+  async create(@Body() createOrderDto: CreateOrderDto): Promise<Response<Promise<void>>> {
     const order = new OrderEntity(
       this._uuidService.generateUuid(),
       createOrderDto.currencyCode,
@@ -21,37 +22,53 @@ export class OrderController {
       new Date(),
       new Date(),
     );
-    return this.orderService.create(order);
+    await this.orderService.create(order);
+    return {
+      message: 'Order created successfully',
+    };
   }
 
   @Get()
-  async findAll(): Promise<OrderEntity[]> {
-    return this.orderService.findAll();
+  async findAll(): Promise<Response<OrderEntity[]>> {
+    const list = await this.orderService.findAll();
+    return {
+      message: 'Orders retrieved successfully',
+      data: list,
+    };
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<OrderEntity> {
+  async findOne(@Param('id') id: string): Promise<Response<OrderEntity>> {
     const order = await this.orderService.findOne(id);
     if (!order) {
       throw new HttpException('Order not found', 404);
     }
-    return order;
+    return {
+      message: 'Order retrieved successfully',
+      data: order,
+    };
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto): Promise<OrderEntity> {
+  async update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto): Promise<Response<OrderEntity>> {
     const possibleUser = await this.orderService.update(id, updateOrderDto);
     if (!possibleUser) {
       throw new HttpException('Order not found', 404);
     }
-    return possibleUser;
+    return {
+      message: 'Order updated successfully',
+      data: possibleUser,
+    };
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<Response<boolean>> {
     const status = await this.orderService.remove(id);
     if (!status) {
       throw new HttpException('Order not found', 404);
     }
+    return {
+      message: 'Order deleted successfully',
+    };
   }
 }
