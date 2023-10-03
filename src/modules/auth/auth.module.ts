21ebaa940425b492c0bfcc5bb9001a5229c40821
devkiloton <dev.kiloton@gmail.com>
2023-10-03 20:50:14 -0300

    Returning jwt when signing in

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { HashService } from 'src/shared/services/hash/hash.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, HashService],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: 'rocess.env]',
+      signOptions: { expiresIn: '48h' },
+    }),
+  ],
 })
 export class AuthModule {}
