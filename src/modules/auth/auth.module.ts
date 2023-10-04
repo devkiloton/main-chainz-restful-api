0243b4d7576973bf4d024c88ca9f00e251de90d7
devkiloton <dev.kiloton@gmail.com>
2023-10-03 22:34:21 -0300

    Finishing jwt implementation

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,16 +4,23 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { HashService } from 'src/shared/services/hash/hash.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, HashService],
   imports: [
     UserModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          global: true,
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '48h' },
+        };
+      },
+      inject: [ConfigService],
       global: true,
-      secret: 'rocess.env]',
-      signOptions: { expiresIn: '48h' },
     }),
   ],
 })
