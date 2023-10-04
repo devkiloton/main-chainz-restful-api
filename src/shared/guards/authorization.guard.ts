aa9f2e3e5eb99ca7a365b00cd1e28df3c4eee7ef
devkiloton <dev.kiloton@gmail.com>
2023-10-03 22:03:13 -0300

    Creating authorization guard

---
+++ src/shared/guards/authorization.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { isNil } from 'lodash';
+import { UserPayload } from 'src/types/user-payload';
+import { UserReq } from 'src/types/user-req';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  constructor(private readonly _jwtService: JwtService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<UserReq>();
+    const access_token = this._tokenExtractor(request);
+    if (!access_token) {
+      throw new UnauthorizedException('Not authorized');
+    }
+
+    try {
+      const payload: UserPayload = await this._jwtService.verifyAsync(access_token);
+      request.user = payload;
+    } catch (error) {
+      throw new UnauthorizedException('Not authorized');
+    }
+
+    return true;
+  }
+
+  private _tokenExtractor(request: Request): string | undefined {
+    const authorization = request.headers.authorization;
+    const [bearer, token] = authorization?.split(' ') ?? [];
+    return bearer === 'Bearer' && !isNil(token) ? token : undefined;
+  }
+}
