c5df8b8ca7d5c81dc1a82018f55f13602d31f1b2
devkiloton <dev.kiloton@gmail.com>
2023-10-05 09:13:16 -0300

    Getting status early

--- src/resources/filters/exception-filter/all-exceptions.filter.ts
+++ src/resources/filters/exception-filter/all-exceptions.filter.ts
@@ -1,14 +1,15 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus, ConsoleLogger } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly _consoleLogger: ConsoleLogger) {}
   catch(exception: unknown, host: ArgumentsHost) {
+    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    this._consoleLogger.error(`${exception} - ${status}`);
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-
-    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
     response.status(status).json({
       statusCode: status,
