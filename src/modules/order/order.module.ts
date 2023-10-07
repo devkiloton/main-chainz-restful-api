d364e3ad740d434ebd9bf4261e11a43608765566
devkiloton <dev.kiloton@gmail.com>
2023-10-07 00:25:20 -0300

    removing unused service

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
-import { UuidService } from 'src/shared/services/uuid/uuid.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { UserEntity } from '../user/entities/user.entity';
@@ -9,6 +8,6 @@ import { UserEntity } from '../user/entities/user.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([OrderEntity, UserEntity])],
   controllers: [OrderController],
-  providers: [OrderService, UuidService],
+  providers: [OrderService],
 })
 export class OrderModule {}
