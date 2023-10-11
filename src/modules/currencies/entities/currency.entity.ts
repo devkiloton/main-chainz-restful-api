a61fb5e012c69740f9ee06babba577c149e272f8
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-11 07:51:06 -0300

    Creating initial model

--- src/modules/currencies/entities/currency.entity.ts
+++ src/modules/currencies/entities/currency.entity.ts
@@ -1,1 +1,25 @@
-export class Currency {}
+import { Column, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'currencies' })
+export class Currency {
+  @PrimaryColumn()
+  public id!: string;
+
+  @Column({ length: 40, nullable: false })
+  public name!: string;
+
+  @Column({ length: 8, nullable: false })
+  public ticker!: string;
+
+  @Column({ nullable: false })
+  public price!: number;
+
+  @Column({ nullable: false })
+  public priceChange24h!: number;
+
+  @Column({ nullable: false })
+  public marketCap!: number;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  public updatedAt!: Date;
+}
