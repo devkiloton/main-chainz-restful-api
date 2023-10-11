429ef7ce1e13705044e338a046fd3a780d8a1909
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-11 08:45:31 -0300

    Putting back db commands

--- src/modules/fiat-currencies/fiat-currencies.service.ts
+++ src/modules/fiat-currencies/fiat-currencies.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateFiatCurrencyDto } from './dto/update-fiat-currency.dto';
+import { CreateFiatCurrencyDto } from './dto/create-fiat-currency.dto';
 
 @Injectable()
 export class FiatCurrenciesService {
+  create(_createCurrencyDto: CreateFiatCurrencyDto) {
+    return 'This action adds a new currency';
+  }
+
   findAll() {
     return `This action returns all fiatCurrencies`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} fiatCurrency`;
+  }
+
+  update(id: number, _updateCurrencyDto: UpdateFiatCurrencyDto) {
+    return `This action updates a #${id} currency`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} currency`;
   }
 }
