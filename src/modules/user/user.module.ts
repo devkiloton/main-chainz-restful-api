36494657ac0c39cb3fb11e30f8302e079cb0919d
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-19 20:42:57 -0300

    importing auth entity

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -6,10 +6,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { HashService } from 'src/shared/services/hash/hash.service';
 import { AuthModule } from '../auth/auth.module';
-import { AuthEntity } from '../auth/entities/auth.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, AuthEntity]), AuthModule],
+  imports: [TypeOrmModule.forFeature([UserEntity, AuthModule])],
   controllers: [UserController],
   providers: [UserService, UniqueEmailValidator, HashService],
   exports: [UserService],
