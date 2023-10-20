b9a67beeb2ce17da25a20bdbfb438b34d58449e1
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-20 01:10:16 -0300

    Improving cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { welcomeMessage } from './resources/helpers/welcome-message';
 import { setupSwagger } from './resources/helpers/setup-swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -14,9 +16,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  if (process.env['NODE_ENV'] === 'development') {
-    app.enableCors();
-  }
+  app.enableCors({
+    origin: ['http://localhost:4200', 'http://localhost:4000'],
+  });
   setupSwagger(app);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env['PORT'] || 3000);
