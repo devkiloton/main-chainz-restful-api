ff2316a3c8b551d019c64ede2ba1dbadb400cca7
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-19 23:54:51 -0300

    Creating mail service

---
+++ src/modules/email/email.service.ts
@@ -0,0 +1,20 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EmailService {
+  constructor(
+    private readonly _mailerService: MailerService,
+    private readonly _configService: ConfigService,
+  ) {}
+  public async sendEmailResetPasswordCode(data: { receiver: string; code: number }): Promise<void> {
+    this._mailerService.sendMail({
+      to: data.receiver,
+      from: this._configService.get<string>('MAILER_USER'),
+      subject: 'Testing Nest MailerModule ✔',
+      text: 'welcome',
+      html: '<h1>welcome</h1>',
+    });
+  }
+}
