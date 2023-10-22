d54ec35a9858c52b357e71a3a5784ed66bcdae3d
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-22 02:32:20 -0300

    Not allowing unverified users

--- src/shared/guards/access-token.guard.ts
+++ src/shared/guards/access-token.guard.ts
@@ -1,5 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { isNotNil } from 'ramda';
+import { UserPayload } from 'src/types/user-payload';
+import { UserReq } from 'src/types/user-req';
 
 @Injectable()
-export class AccessTokenGuard extends AuthGuard('jwt') {}
+export class AccessTokenGuard extends AuthGuard('jwt') {
+  constructor(private readonly _jwtService: JwtService) {
+    super();
+  }
+
+  override async canActivate(context: ExecutionContext): Promise<any> {
+    const request = context.switchToHttp().getRequest<UserReq>();
+    const access_token = this._tokenExtractor(request);
+    if (!access_token) {
+      throw new UnauthorizedException('Not authorized');
+    }
+
+    try {
+      const payload: UserPayload = await this._jwtService.verifyAsync(access_token);
+      if (!payload.isEmailVerified) {
+        throw new UnauthorizedException('Not authorized');
+      }
+    } catch (error) {
+      throw new UnauthorizedException('Not authorized');
+    }
+
+    return super.canActivate(context);
+  }
+
+  private _tokenExtractor(request: Request): string | undefined {
+    const authorization = request.headers.authorization;
+    const [bearer, token] = authorization?.split(' ') ?? [];
+    return bearer === 'Bearer' && isNotNil(token) ? token : undefined;
+  }
+}
