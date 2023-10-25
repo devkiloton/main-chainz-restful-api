2c9711ac56386455fcdf44a31e7c66b07e20e8bd
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-24 22:41:22 -0300

    Creating enums

---
+++ src/modules/articles/enum/category.ts
@@ -0,0 +1,14 @@
+export enum Category {
+  my_account = 'my-account',
+  deposit_and_withdrawal = 'deposit-and-withdrawal',
+  security = 'security',
+  anonimity = 'anonimity',
+  p2p = 'p2p',
+  policies = 'policies',
+  transfer_crypto = 'transfer-crypto',
+  contact = 'contact',
+  buy_and_sell = 'buy-and-sell',
+  getting_started = 'getting-started',
+  trading = 'trading',
+  terms = 'terms',
+}
