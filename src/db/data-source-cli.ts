29a6b6a5acd629d27b1c759c362b440628c248e2
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-29 22:28:19 -0300

    Updating datasources for mysql

--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -3,13 +3,17 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: process.env['DB_HOST'],
-  port: Number(process.env['DB_PORT']),
+  url: process.env['DB_HOST'],
+  ssl: {
+    // #TODO: Provide certificate for SSL connection later
+    rejectUnauthorized: false,
+  },
   username: process.env['DB_USERNAME'],
   password: process.env['DB_PASSWORD'],
   database: process.env['DB_DATABASE'],
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/migrations/*.{js,ts}'],
+  synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
