31754c969e4b4aff0965775b0d34d63810ebef7b
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-29 22:23:54 -0300

    Documenting bitcoin service

--- src/modules/bitcoin/bitcoin.service.ts
+++ src/modules/bitcoin/bitcoin.service.ts
@@ -20,6 +20,13 @@ export class BitcoinService {
     private readonly _configService: ConfigService,
     private readonly _walletsService: WalletsService,
   ) {}
+
+  /**
+   * Creates a wallet for a user.
+   *
+   * @param data - The `userId` of the user to create a wallet for.
+   * @returns The `seed` and `address` of the created wallet.
+   */
   async createWallet(data: { userId: string }): Promise<CreatedWallet> {
     const network = this._configService.get<'mainnet' | 'testnet'>('BITCOIN_NETWORK');
     const networkConfig = network === 'mainnet' ? bitcore.Networks.mainnet : bitcore.Networks.testnet;
@@ -36,34 +43,69 @@ export class BitcoinService {
     };
   }
 
+  /**
+   *
+   * @param data - The `receiver` and `satoshis` of the transaction to create and the `userId` of the user that is creating the transaction.
+   * @returns The `txid` of the created transaction.
+   * @throws `Error` - If the balance of the wallet is not enough to create the transaction.
+   */
   async createTransaction(data: { receiver: string; satoshis: number; userId: string }) {
     const network = this._configService.get<'mainnet' | 'testnet'>('BITCOIN_NETWORK');
     const networkConfig = network === 'mainnet' ? bitcore.Networks.mainnet : bitcore.Networks.testnet;
+
+    // Get the wallet of the user that is creating the transaction.
     const wallet = await this._walletsService.getWallet({ userId: data.userId, currencyCode: SupportedCurrencies.btc });
+
+    // Take and decrypt the seed of the wallet.
     const encryptedSeed = wallet.seed;
     const decryptedSeed = decryption(encryptedSeed);
+
+    // Create a code from the seed.
     const code = new Mnemonic(decryptedSeed);
+
+    // Get the information of the wallet.
     const info = infoFromSeed({ code, network: networkConfig });
+
+    // Get the utxos of the wallet.
     const utxos: UTXOs = await axios
       .get(`${this._configService.get('BITCOIN_API')}/${network}/address/${info.address}/?unspent=true`)
       .then(res => res.data);
+
+    // Create a transaction.
     const transaction = new bitcore.Transaction();
+
+    // Map the utxos to inputs and add them to the transaction.
     const inputs: Array<bitcore.Transaction.UnspentOutput> = mapUtxosFromAPI({ utxos, network: networkConfig });
     transaction.from(inputs);
+
+    // Calculate the fees of the transaction.
     const transactionSize = inputs.length * 180 + 2 * 34 + 10 - inputs.length;
+
+    // Get the fees from the API and calculate the recommended fee of the transaction.
     const fees: Fees = await axios.get(`${this._configService.get('BITCOIN_FEE_API')}`).then(res => res.data);
     const fee = fees.priority * transactionSize;
 
+    // Check if the balance of the wallet is enough to create the transaction.
     const balance: Balance = await axios
       .get(`${this._configService.get('BITCOIN_API')}/${network}/address/${info.address}/balance`)
       .then(res => res.data);
+
+    // If the balance is not enough, throw an error.
     if (!(balance.balance >= data.satoshis + fee)) {
       throw new Error('Not enough balance');
     }
+
+    // Adds the fee and the receiver to the transaction.
     transaction.fee(fee);
     transaction.to(data.receiver, data.satoshis);
+
+    // Set change address
     transaction.change(info.address);
+
+    // Sign the transaction
     transaction.sign(info.privateKeyStr);
+
+    // Serialize the transaction and send it to the API.
     const serializedTransaction = transaction.serialize();
     const transactionAPI: {
       txid: string;
@@ -74,9 +116,16 @@ export class BitcoinService {
         rawTx: serializedTransaction,
       },
     }).then(res => res.data);
+
+    // Return the txid of the transaction.
     return transactionAPI.txid;
   }
 
+  /**
+   *
+   * @param userId - The `userId` of the user to get the wallet information from.
+   * @returns The `balance`, `address`, `unconfirmed` and `qrcode` of the wallet.
+   */
   async findInformation(userId: string): Promise<PublicWallet> {
     const network = this._configService.get<'mainnet' | 'testnet'>('BITCOIN_NETWORK');
     const networkConfig = network === 'mainnet' ? bitcore.Networks.mainnet : bitcore.Networks.testnet;
